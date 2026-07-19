3e6dc9f41d5a44b1d4469d6d81d820f34f56140e
Landon Maynard <landonmaynard13@gmail.com>
2026-07-19 10:44:35 -0600

    Implement video recording for ascent and descent
    Added camera functionality to record ascent and descent videos, including SD card integration and frame size adjustments.

--- software/main.cpp
+++ software/main.cpp
@@ -2,6 +2,28 @@
 #include <SparkFunBME280.h>
 #include <esp_camera.h>
 #include <Wire.h>
+#include <FS.h>
+#include <SD_MMC.h>
+#include <soc/soc.h>
+#include <soc/rtc_cntl_reg.h>
+
+#define PWDN_GPIO_NUM     32
+#define RESET_GPIO_NUM    -1
+#define XCLK_GPIO_NUM      0
+#define SIOD_GPIO_NUM     26
+#define SIOC_GPIO_NUM     27
+
+#define Y9_GPIO_NUM       35
+#define Y8_GPIO_NUM       34
+#define Y7_GPIO_NUM       39
+#define Y6_GPIO_NUM       36
+#define Y5_GPIO_NUM       21
+#define Y4_GPIO_NUM       19
+#define Y3_GPIO_NUM       18
+#define Y2_GPIO_NUM        5
+#define VSYNC_GPIO_NUM    25
+#define HREF_GPIO_NUM     23
+#define PCLK_GPIO_NUM     22
 
 BME280 bmp;
 
@@ -34,7 +56,6 @@ int landingDetectionDelay = landingDetectionHZ/1000; //how much to wait in betwe
 unsigned long landingPrevMillis;
 
 //saving altitudes to ram
-
 struct bmpData{
   float altF;
   float tempF;
@@ -47,25 +68,143 @@ int currentIndex = 0;
 int bmpDataHZ = 60; //how many times per second to save altitude data to ram
 int bmpDataDelay = 1000/bmpDataHZ; //how many milliseconds to wait between saving data to ram
 
+//video vars
+File videoFile;
+bool recording = false;
+bool recordingDescent = false;
+unsigned long lastFrameMillis = 0;
+const int videoFPS = 30;
+const unsigned long videoFrameDelay = 1000/videoFPS; //how many milliseconds to wait between frames
+
 // put function declarations here:
 bool detectLaunch();
 bool detectApogee();
 bool detectLanding();
 
+void bmpTaskCode(void *parameter);
+
 
 void setup() {
   // put your setup code here, to run once:
+
+  Wire.begin(1, 3);
+  bmp.beginI2C();
+
+WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0);
   psramInit();
+
+camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sccb_sda = SIOD_GPIO_NUM;
+  config.pin_sccb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG;
+
+  config.frame_size = FRAMESIZE_UXGA; // FRAMESIZE_ + QVGA|CIF|VGA|SVGA|XGA|SXGA|UXGA
+  config.jpeg_quality = 10;
+  config.fb_count = 2;
+  
+  // Init Camera
+  esp_err_t err = esp_camera_init(&config);
+
+  if (err == ESP_OK) {
+  sensor_t * s = esp_camera_sensor_get();
+  // Drop down to VGA for the high-FPS ascent video recording
+  s->set_framesize(s, FRAMESIZE_VGA);
+  s->set_brightness(s, -1);     // -2 to 2 **************************
+  s->set_contrast(s, 0);       // -2 to 2
+  s->set_saturation(s, 0);     // -2 to 2
+  s->set_special_effect(s, 0); // 0 to 6 (0 - No Effect, 1 - Negative, 2 - Grayscale, 3 - Red Tint, 4 - Green Tint, 5 - Blue Tint, 6 - Sepia)
+  s->set_whitebal(s, 1);       // 0 = disable , 1 = enable
+  s->set_awb_gain(s, 1);       // 0 = disable , 1 = enable
+  s->set_wb_mode(s, 0);        // 0 to 4 - if awb_gain enabled (0 - Auto, 1 - Sunny, 2 - Cloudy, 3 - Office, 4 - Home)
+  s->set_exposure_ctrl(s, 1);  // 0 = disable , 1 = enable
+  s->set_aec2(s, 0);           // 0 = disable , 1 = enable
+  s->set_ae_level(s, 0);       // -2 to 2
+  s->set_aec_value(s, 300);    // 0 to 1200
+  s->set_gain_ctrl(s, 1);      // 0 = disable , 1 = enable
+  s->set_agc_gain(s, 0);       // 0 to 30
+  s->set_gainceiling(s, (gainceiling_t)0);  // 0 to 6
+  s->set_bpc(s, 0);            // 0 = disable , 1 = enable
+  s->set_wpc(s, 1);            // 0 = disable , 1 = enable
+  s->set_raw_gma(s, 1);        // 0 = disable , 1 = enable
+  s->set_lenc(s, 1);           // 0 = disable , 1 = enable
+  s->set_hmirror(s, 0);        // 0 = disable , 1 = enable
+  s->set_vflip(s, 0);          // 0 = disable , 1 = enable
+  s->set_dcw(s, 1);            // 0 = disable , 1 = enable
+  s->set_colorbar(s, 0);       // 0 = disable , 1 = enable 
+  }
+
+  SD_MMC.begin();
+  uint8_t cardType = SD_MMC.cardType();
+
+  camera_fb_t * fb = NULL;
+
   bmpDataArray = (bmpData*)ps_malloc(sizeof(bmpData) * bmpDataArraySize);
-  void bmpTaskCode(void *parameter);
   xTaskCreatePinnedToCore(bmpTaskCode, "bmpTask", 10000, NULL, 0, &bmpTask, 0);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  //Camera task
-}
+  if(onGround && !recording && !apogeeDetected){
+    videoFile = SD_MMC.open("/ascent.avi", FILE_WRITE);
+    if(videoFile){
+      recording = true;
+      lastFrameMillis = millis();
+    }
+  }
+  if(recording && !apogeeDetected){
+    if(millis() - lastFrameMillis >= videoFrameDelay){
+      lastFrameMillis = millis();
+      camera_fb_t * fb = esp_camera_fb_get();
+      if(fb){
+        videoFile.write(fb->buf, fb->len);
+        esp_camera_fb_return(fb);
+      }
+    }
+  }
+  if(apogeeDetected && recording){
+    videoFile.close();
+    recording = false;
 
+    sensor_t * s = esp_camera_sensor_get();
+    s->set_framesize(s, FRAMESIZE_UXGA); //return to UXGA for the high-res descent video recording
+    delay(250); //wait for camera to adjust to new resolution
+  }
+  if(apogeeDetected && !recordingDescent && !landingDetected){
+    videoFile = SD_MMC.open("/descent.avi", FILE_WRITE);
+    if(videoFile){
+      recordingDescent = true;
+      lastFrameMillis = millis();
+    }
+  }
+  if(recordingDescent && !landingDetected){
+    camera_fb_t * fb = esp_camera_fb_get();
+    if(fb){
+      videoFile.write(fb->buf, fb->len);
+      esp_camera_fb_return(fb);
+    }
+  }
+  if(landingDetected && recordingDescent){
+    videoFile.close();
+    recordingDescent = false;
+  }
+}
 // put function definitions here:
 
 bool detectLaunch(){
@@ -77,7 +216,7 @@ bool detectLaunch(){
 }
 
 bool detectApogee(){
-  if(previousAlt - currentAlt < apogeeDetectionThreshold){
+  if(previousAlt - currentAlt > apogeeDetectionThreshold){
     return true;
   }else{
     return false;
